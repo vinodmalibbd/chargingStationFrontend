7de060d2953a0592a68e5481df9245a6f8f4034d
vinodmalibbd <vinod.mali@bbd.co.za>
2024-05-13 01:45:29 +0530

    show all chargepoints functionality

--- src/Pages/User/ShowChargingStation.js
+++ src/Pages/User/ShowChargingStation.js
@@ -17,30 +17,4 @@ function showAllChargingStations(){
         })
       })
       document.body.appendChild(cardContainer);
-
-      // stations.map((item )=>{
-      
-      // });
-
-      // for(var i=0;i<stations.length;i++){
-      //   const station = stations[i];
-
-      //   const card = document.createElement('div');
-      //   card.classList.add('card');
-
-      //   const name = document.createElement('h3');
-      //   name.textContent = station.name;
-
-      //   const location = document.createElement('p');
-      //   location.textContent = `Location: ${station.location}`;
-
-      //   const availability = document.createElement('p');
-      //   availability.textContent = `Availability: ${station.availability}`;
-
-      //   card.appendChild(name);
-      //   card.appendChild(location);
-      //   card.appendChild(availability);
-
-      //   cardContainer.appendChild(card);
-      // }
 }
