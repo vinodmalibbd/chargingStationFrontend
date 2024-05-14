13a497a5d5c4e91057fb5d15f8acac6ca9e178c6
vinodmalibbd <vinod.mali@bbd.co.za>
2024-05-14 12:59:45 +0530

    adding workflow

--- src/service/evstationService.js
+++ src/service/evstationService.js
@@ -1,126 +1,137 @@
-const getAllChargingStation=async()=>{
-    const res =await fetch(`http://localhost:8081/chargingstation/all`,{
-        method:"GET",
-        headers:{
-            'Content-Type':"application/json"
-        }
-    })
-    return res.json()
+const getAllChargingStation = async () => {
+  const res = await fetch(
+    `http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/chargingstation/all`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return res.json();
 };
-const loginchargingStation =async()=>{
-    window.location.href='http://localhost:8081/auth/chargingstation';
+const loginchargingStation = async () => {
+  window.location.href =
+    "http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/auth/chargingstation";
 };
-const loginuser =async()=>{
-    window.location.href='http://localhost:8081/auth/user';
+const loginuser = async () => {
+  window.location.href =
+    "http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/auth/user";
 };
-const LoginUser =async()=>{
-    const res=await fetch('http://localhost:8081/auth/getuser',{
-        method:"GET",
-        headers:{
-            'Content-Type':"application/json"
+const LoginUser = async () => {
+  const res = await fetch(
+    "http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/auth/getuser",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return res;
+};
+const getChargingStationById = async () => {
+  const token = localStorage.getItem("station-cookie");
+  if (token) {
+    const decodedtoken = decodeJwtToken(token);
+    const stationId = decodedtoken.sub;
+    const res = await fetch(
+      `http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/chargingstation/${stationId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
         },
-    
-    })
-    return res;
+      }
+    );
+    return res.json();
+  } else {
+    navigateTo("/");
+  }
 };
-const getChargingStationById =async()=>{
-    const token =localStorage.getItem('station-cookie')
-    if(token){
-
-        const decodedtoken=decodeJwtToken(token);
-        const stationId=decodedtoken.sub;
-        const res=await fetch(`http://localhost:8081/chargingstation/${stationId}`,{
-            method:"GET",
-            headers:{
-                'Content-Type':"application/json",
-                'Authorization':token
-            },
-        
-        })
-        return res.json();
-    }else{
-        navigateTo('/')
+const updateChargingStationProfile = async (data) => {
+  const token = localStorage.getItem("station-cookie");
+  if (token) {
+    const decodedtoken = decodeJwtToken(token);
+    const stationId = decodedtoken.sub;
+    const res = await fetch(
+      `http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/chargingstation/update`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    if (res.status === 201) {
+      console.log(res.json());
+      navigateTo("/station");
     }
+  } else {
+    navigateTo("/");
+  }
 };
-const updateChargingStationProfile =async(data)=>{
-    const token =localStorage.getItem('station-cookie')
-    if(token){
+const getAllChargingStationSlots = async () => {
+  const token = localStorage.getItem("station-cookie");
+  if (token) {
+    const decodedtoken = decodeJwtToken(token);
+    const stationId = decodedtoken.sub;
+    const res = await fetch(
+      `http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/chargingslot/all/${stationId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      }
+    );
 
-        const decodedtoken=decodeJwtToken(token);
-        const stationId=decodedtoken.sub;
-        const res=await fetch(`http://localhost:8081/chargingstation/update`,{
-            method:"PUT",
-            headers:{
-                'Content-Type':"application/json",
-                'Authorization':token,
-            },
-            body:JSON.stringify(data)
-        
-        })
-        if(res.status===201){
-
-            console.log(res.json());
-            navigateTo('/station')
-
-        }
-    }else{
-        navigateTo('/')
+    if (res.ok) {
+      return res.json();
     }
+  } else {
+    navigateTo("/");
+  }
 };
-const getAllChargingStationSlots =async()=>{
-    const token =localStorage.getItem('station-cookie')
-    if(token){
-
-        const decodedtoken=decodeJwtToken(token);
-        const stationId=decodedtoken.sub;
-        const res=await fetch(`http://localhost:8081/chargingslot/all/${stationId}`,{
-            method:"GET",
-            headers:{
-                'Content-Type':"application/json",
-                'Authorization':token
-            },
-        })
-        
-        if(res.ok){
-
-            return res.json();
-        }
-    }else{
-        navigateTo('/')
+const addChargingSlotStation = async (data) => {
+  const token = localStorage.getItem("station-cookie");
+  if (token) {
+    const decodedtoken = decodeJwtToken(token);
+    const stationId = decodedtoken.sub;
+    const res = await fetch(
+      `http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/chargingslot/addslot/${stationId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    if (res.status === 201) {
+      navigateTo("/station");
+      console.log(res.json());
+      return res.json();
     }
+  } else {
+    navigateTo("/");
+  }
 };
-const addChargingSlotStation =async(data)=>{
-    const token =localStorage.getItem('station-cookie')
-    if(token){
-        const decodedtoken=decodeJwtToken(token);
-        const stationId=decodedtoken.sub;
-        const res=await fetch(`http://localhost:8081/chargingslot/addslot/${stationId}`,{
-            method:"POST",
-            headers:{
-                'Content-Type':"application/json",
-                'Authorization':token
-            },
-            body:JSON.stringify(data)
-        })
-        if(res.status===201){
-            navigateTo('/station')
-            console.log( res.json());
-            return res.json();
-        }
-    }else{
-        navigateTo('/')
+const getAllChargingStationSlotsById = async (id) => {
+  const res = await fetch(
+    `http://ec2-54-75-101-91.eu-west-1.compute.amazonaws.com:8081/chargingslot/all/${id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     }
-};
-const getAllChargingStationSlotsById =async(id)=>{
+  );
 
-        const res=await fetch(`http://localhost:8081/chargingslot/all/${id}`,{
-            method:"GET",
-            headers:{
-                'Content-Type':"application/json"
-            },
-        })
-        
-
-
-            return res.json();
+  return res.json();
 };
