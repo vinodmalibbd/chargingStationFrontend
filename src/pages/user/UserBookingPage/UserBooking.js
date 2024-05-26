e6a11f35b7993d65bf8fae211ee2b5ed403550a5
BBDNET\bbdnet10273 <Mohan.Muntala@bbd.co.za>
2024-05-26 22:00:20 +0530

    implemented user booking functionality

--- src/pages/user/UserBookingPage/UserBooking.js
+++ src/pages/user/UserBookingPage/UserBooking.js
@@ -1,10 +1,15 @@
 let bookingRequest = {
-  userId: null,
-  chargingSlotId: null,
-  timeSlotId: null,
   date: null,
+  chargingSlotId: null,
+  stationId: null,
 };
-
+ 
+let getslotrequest = {
+  date: null,
+  chargingSlotId: null,
+  stationId: null,
+};
+ 
 function getCurrentDate() {
   const today = new Date();
   const year = today.getFullYear();
@@ -12,171 +17,172 @@ function getCurrentDate() {
   const day = String(today.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 }
-
-async function userBooking() {
-  // let chargingSlots = getAllChargingStationSlots
-
+ 
+function userBooking(chargepoint) {
+  console.log(chargepoint);
+ 
   const BookingDiv = document.querySelector(".main_content");
   BookingDiv.innerHTML = "";
-
+ 
   const headingDiv = document.createElement("div");
   headingDiv.className = "BookingHeading";
-
+ 
   const bookingHeader = document.createElement("span");
   bookingHeader.className = "bookingSpan";
-  bookingHeader.textContent = "Kalyani Charging Point";
-
+  bookingHeader.textContent = `${chargepoint.name}`;
+ 
   const stationAdderess = document.createElement("span");
-  stationAdderess.textContent =
-    "Fortaleza Complex, 5, Central Ave, Princeton Town Society, Prathamesh Society, Kalyani Nagar, Pune, Maharashtra 411006";
-
+  getAddress(chargepoint.latitude, chargepoint.longitude).then(
+    (addr) => (stationAdderess.textContent = addr)
+  );
+ 
   headingDiv.appendChild(bookingHeader);
   headingDiv.appendChild(stationAdderess);
   BookingDiv.appendChild(headingDiv);
-
+ 
   const chargePointCardsDiv = document.createElement("div");
   chargePointCardsDiv.className = "chargePointCardsDiv";
   BookingDiv.appendChild(chargePointCardsDiv);
-  const stationID = 1;
-
-  const data = await getAllChargingStationSlots(stationID);
-  if (data) {
-    console.log(data);
-    data.map((slots) => {
-      const chargePointcard = document.createElement("div");
-      chargePointcard.className = "chargePointcard";
-
-      const chargePointInfo = document.createElement("div");
-      chargePointInfo.className = "chargePointInfo";
-
-      const chargepointId = document.createElement("span");
-      chargepointId.textContent = "Charging Point: ";
-
-      const chargepointIdNo = document.createElement("span");
-      chargepointIdNo.textContent = "1";
-      chargepointIdNo.className = "sp1";
-      chargepointId.appendChild(chargepointIdNo);
-
-      const Price = document.createElement("span");
-      Price.textContent = "Price :";
-
-      const PriceAmount = document.createElement("span");
-      PriceAmount.textContent = " ₹200";
-      PriceAmount.className = "sp1";
-      Price.appendChild(PriceAmount);
-      chargePointInfo.appendChild(chargepointId);
-      chargePointInfo.appendChild(Price);
-      chargePointcard.appendChild(chargePointInfo);
-
-      const bookingDatePickerDiv = document.createElement("div");
-      bookingDatePickerDiv.className = "bookingDatePickerDiv";
-
-      const bookingDatePicker = document.createElement("input");
-      bookingDatePicker.className = "bookingDatePicker";
-      bookingDatePicker.type = "date";
-      bookingDatePicker.value = getCurrentDate();
-      bookingDatePicker.min = getCurrentDate();
-      bookingDatePicker.onchange = () => {
-        generateTimeSlot(bookingRequest);
-      };
-
-      const DatePickerlabel = document.createElement("label");
-      DatePickerlabel.className = "UserDatePickerSpan";
-      DatePickerlabel.textContent = "Date : ";
-
-      bookingDatePickerDiv.appendChild(DatePickerlabel);
-      bookingDatePickerDiv.appendChild(bookingDatePicker);
-      chargePointcard.appendChild(bookingDatePickerDiv);
-
-      const BookingtimeslotDiv = document.createElement("div");
-      BookingtimeslotDiv.className = "BookingtimeslotDiv";
-
-      const timeSlotlabel = document.createElement("label");
-      timeSlotlabel.className = "timeSlotlabel";
-      timeSlotlabel.textContent = "Time Slot : ";
-
-      const timeSlotList = document.createElement("select");
-      timeSlotList.className = "timeSlotList";
-      getSlotAvailblity(bookingRequest).then((data) => {
-        data.map((timeslot) => {
-          const timeslotoption = document.createElement("option");
-          timeslotoption.value = timeslot.Id;
-          timeslotoption.textContent = `${startTime}:00 To ${endTime}:00`;
-          timeSlotList.appendChild(timeslotoption);
-        });
+  getslotrequest.stationId = chargepoint.stationId;
+ 
+  getAllChargingStationSlotsById(chargepoint.stationId).then((data) => {
+    if (data) {
+      console.log(data);
+      data.map((slots) => {
+        generateChargPoints(slots);
       });
-
-      BookingtimeslotDiv.appendChild(timeSlotlabel);
-      BookingtimeslotDiv.appendChild(timeSlotList);
-      chargePointcard.appendChild(BookingtimeslotDiv);
-
-      const BookingButtonGroup = document.createElement("div");
-      BookingButtonGroup.className = "BookingButtonGroup";
-
-      const userBookingButton = document.createElement("button");
-      userBookingButton.className = "BookingButton";
-      userBookingButton.textContent = "Book Now ";
-
-      BookingButtonGroup.appendChild(userBookingButton);
-      chargePointcard.appendChild(BookingButtonGroup);
-      chargePointCardsDiv.appendChild(chargePointcard);
-    });
-  }
-
+    }
+  });
+ 
   const UserFeedback = document.createElement("div");
-  UserFeedback.className = "UserFeedback";
+  UserFeedback.className = "UserBookingFeedback";
   BookingDiv.appendChild(UserFeedback);
 }
-
-function createChargePointCards(chargingStation) {
-  const slotsCards = document.querySelector(".chargePointDiv");
-  // getAllChargingStationSlotsById(1).then(slots =>{
-
-  //   console.log(slots);
-  //   if(slots && slots.length > 0){
-  //     slots.map(slot => {
-  //       const chargingSlots = document.createElement('div');
-  //       chargingSlots.className = 'charginSlot';
-
-  //       const chargingSlotId = document.createElement('span');
-  //       chargingSlotId.className = 'chargingSlotId';
-  //       chargingSlotId.textContent = `Slot : ${slot.slotId}`;
-
-  //       const chargingSlotPrice = document.createElement('span');
-  //       chargingSlotPrice.className = 'chargingSlotPrice';
-  //       chargingSlotPrice.textContent = `Price : ₹${slot.pricePerHour}`;
-
-  //       chargingSlots.appendChild(chargingSlotId);
-  //       chargingSlots.appendChild(chargingSlotPrice);
-
-  //       slotsCards.appendChild(chargingSlots);
-  //   });
-  //   }
-  //   else{
-  //      const noslotAvailableslotsCards = document.createElement('div');
-  //      noslotAvailableslotsCards.className = 'noslotAvailableslotsCards';
-
-  //      const warningspan = document.createElement('span');
-  //      warningspan.textContent = 'Warning';
-
-  //      const warningspan01 = document.createElement('span');
-  //      warningspan01.textContent ='No Chrging Slot Available for this Station';
-
-  //      noslotAvailableslotsCards.appendChild(warningspan);
-  //      noslotAvailableslotsCards.appendChild(warningspan01);
-  //      chargePointDiv.appendChild(noslotAvailableslotsCards);
-  //   }
-  // });
+ 
+function generateChargPoints(slots) {
+  bookingRequest.chargingSlotId = slots.slotId;
+  const chargePointcard = document.createElement("div");
+  chargePointcard.className = "chargePointcard";
+ 
+  const chargePointInfo = document.createElement("div");
+  chargePointInfo.className = "chargePointInfo";
+ 
+  const chargepointId = document.createElement("span");
+  chargepointId.textContent = "Charging Point: ";
+ 
+  const chargepointIdNo = document.createElement("span");
+  chargepointIdNo.textContent = `${slots.slotId}`;
+  chargepointIdNo.className = "sp1";
+  chargepointId.appendChild(chargepointIdNo);
+ 
+  const Price = document.createElement("span");
+  Price.textContent = "Price :";
+ 
+  const PriceAmount = document.createElement("span");
+  PriceAmount.textContent = `₹${slots.pricePerHour}`;
+  PriceAmount.className = "sp1";
+  Price.appendChild(PriceAmount);
+  chargePointInfo.appendChild(chargepointId);
+  chargePointInfo.appendChild(Price);
+  chargePointcard.appendChild(chargePointInfo);
+ 
+  const bookingDatePickerDiv = document.createElement("div");
+  bookingDatePickerDiv.className = "bookingDatePickerDiv";
+ 
+  const bookingDatePicker = document.createElement("input");
+  bookingDatePicker.className = "bookingDatePicker";
+  bookingDatePicker.type = "date";
+  bookingDatePicker.value = getCurrentDate();
+  bookingDatePicker.min = getCurrentDate();
+ 
+  const DatePickerlabel = document.createElement("label");
+  DatePickerlabel.className = "UserDatePickerSpan";
+  DatePickerlabel.textContent = "Date : ";
+ 
+  bookingDatePickerDiv.appendChild(DatePickerlabel);
+  bookingDatePickerDiv.appendChild(bookingDatePicker);
+  chargePointcard.appendChild(bookingDatePickerDiv);
+ 
+  const BookingtimeslotDiv = document.createElement("div");
+  BookingtimeslotDiv.className = "BookingtimeslotDiv";
+ 
+  const timeSlotlabel = document.createElement("label");
+  timeSlotlabel.className = "timeSlotlabel";
+  timeSlotlabel.textContent = "Time Slot : ";
+ 
+  const timeSlotList = document.createElement("select");
+  timeSlotList.className = "timeSlotList";
+  BookingtimeslotDiv.appendChild(timeSlotlabel);
+  BookingtimeslotDiv.appendChild(timeSlotList);
+ 
+  chargePointcard.appendChild(BookingtimeslotDiv);
+ 
+  const BookingButtonGroup = document.createElement("div");
+  BookingButtonGroup.className = "BookingButtonGroup";
+ 
+  const userBookingButton = document.createElement("button");
+  userBookingButton.className = "BookingButton";
+  userBookingButton.textContent = "Book Now ";
+  userBookingButton.onclick = () => {
+    const selectedTimeslot = timeSlotList.value;
+    console.log(selectedTimeslot);
+    console.log(bookingDatePicker.value);
+    console.log(slots.slotId);
+ 
+    const token = sessionStorage.getItem("web-vb-token");
+    if (token) {
+      const decodedtoken = decodeJwtToken(token);
+      const userId = decodedtoken.sub;
+      const data = {
+        date: new Date(bookingDatePicker.value),
+        chargingSlotId: Number(slots.slotId),
+        userId: Number(userId),
+        timeSlotId: Number(timeSlotList.value),
+      };
+      console.log(data);
+      createBooking(data).then((res) => {
+        console.log(res);
+        createSucessPopUpBox("Thank you for using our service");
+      });
+    } else {
+      createPOPUP("user");
+    }
+  };
+ 
+  BookingButtonGroup.appendChild(userBookingButton);
+  chargePointcard.appendChild(BookingButtonGroup);
+ 
+  const chargePointCardsDiv = document.querySelector(".chargePointCardsDiv");
+  chargePointCardsDiv.appendChild(chargePointcard);
+ 
+  bookingDatePicker.onchange = () => {
+    generateTimeSlot(slots.slotId, bookingDatePicker.value, timeSlotList);
+  };
+ 
+  generateTimeSlot(slots.slotId, bookingDatePicker.value, timeSlotList);
 }
-
-function generateTimeSlot(bookingRequest) {
-  const timeSlotList = document.querySelector(".timeSlotList");
-  getSlotAvailblity(bookingRequest).then((data) => {
-    data.map((timeslot) => {
-      const timeslotoption = document.createElement("option");
-      timeslotoption.value = timeslot.Id;
-      timeslotoption.textContent = `${startTime}:00 To ${endTime}:00`;
-      timeSlotList.appendChild(timeslotoption);
+ 
+async function generateTimeSlot(slotId, date, timeSlotList) {
+  timeSlotList.innerHTML = "";
+  getslotrequest.chargingSlotId = slotId;
+  getslotrequest.date = date;
+  try {
+    const data = await getSlotAvailblity(getslotrequest);
+    const currentdate = new Date();
+    const currentHour = currentdate.getHours();
+    data.forEach((timeslot) => {
+      const startHour = timeslot.startTime;
+      const endHour = timeslot.endTime;
+      if (
+        new Date(date).setHours(startHour) >= currentdate.setHours(currentHour)
+      ) {
+        const timeslotoption = document.createElement("option");
+        timeslotoption.value = timeslot.timeSlotId;
+        timeslotoption.textContent = `${timeslot.startTime}:00 To ${timeslot.endTime}:00`;
+        timeSlotList.appendChild(timeslotoption);
+      }
     });
-  });
+  } catch (error) {
+    console.error("Error fetching time slots:", error);
+  }
 }
