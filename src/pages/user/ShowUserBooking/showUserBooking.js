1babb066d8a65379420110b99297da6d85d4ae60
Harshal Desale <harshal.desale@bbd.co.za>
2024-05-27 01:20:38 +0530

    Added url changes in login controller

--- src/pages/user/ShowUserBooking/showUserBooking.js
+++ src/pages/user/ShowUserBooking/showUserBooking.js
@@ -1,90 +1,97 @@
 function userShowBooking() {
   console.log("hello");
-  const ShowUserBookingMainDiv = document.querySelector('.main_content');
-  ShowUserBookingMainDiv.innerHTML = '';
+  const ShowUserBookingMainDiv = document.querySelector(".main_content");
+  ShowUserBookingMainDiv.innerHTML = "";
 
-  const showUserBookingDiv = document.createElement('div');
-  showUserBookingDiv.className = 'showUserBookingDiv';
+  const showUserBookingDiv = document.createElement("div");
+  showUserBookingDiv.className = "showUserBookingDiv";
   ShowUserBookingMainDiv.appendChild(showUserBookingDiv);
 
-  const ShowUserBookingHeadingSpan = document.createElement('span');
-  ShowUserBookingHeadingSpan.className = 'ShowUserBookingHeadingSpan';
-  ShowUserBookingHeadingSpan.textContent = 'Your Bookings';
+  const ShowUserBookingHeadingSpan = document.createElement("span");
+  ShowUserBookingHeadingSpan.className = "ShowUserBookingHeadingSpan";
+  ShowUserBookingHeadingSpan.textContent = "Your Bookings";
   ShowUserBookingMainDiv.appendChild(ShowUserBookingHeadingSpan);
 
-  const showAllUserBookings = document.createElement('div');
-  showAllUserBookings.className = 'showAllUserBookings';
+  const showAllUserBookings = document.createElement("div");
+  showAllUserBookings.className = "showAllUserBookings";
   ShowUserBookingMainDiv.appendChild(showAllUserBookings);
 
-  showLoggedInUserBookings().then(res => {
-    res.forEach(item => {
-      const ShowUserBookingCard = document.createElement('div');
-      ShowUserBookingCard.className = 'ShowUserBookingCard';
-      showAllUserBookings.appendChild(ShowUserBookingCard);
+  showLoggedInUserBookings()
+    .then((res) => {
+      res.forEach((item) => {
+        const ShowUserBookingCard = document.createElement("div");
+        ShowUserBookingCard.className = "ShowUserBookingCard";
+        showAllUserBookings.appendChild(ShowUserBookingCard);
 
-      const stationNameSpan = document.createElement('span');
-      stationNameSpan.className = 'stationNameSpan';
-      stationNameSpan.innerHTML = `<strong>Chargestation: </strong> ${item.chargingStation.name}`;
-      ShowUserBookingCard.appendChild(stationNameSpan);
+        const stationNameSpan = document.createElement("span");
+        stationNameSpan.className = "stationNameSpan";
+        stationNameSpan.innerHTML = `<strong>Chargestation: </strong> ${item.chargingStation.name}`;
+        ShowUserBookingCard.appendChild(stationNameSpan);
 
-      const stationAddress = document.createElement('span');
-      stationAddress.className = 'stationAddress';
-      getAddress(item.chargingStation.latitude, item.chargingStation.longitude).then(Address => {
-        stationAddress.innerHTML = `<strong>Address:</strong> ${Address}`;
+        const stationAddress = document.createElement("span");
+        stationAddress.className = "stationAddress";
+        getAddress(
+          item.chargingStation.latitude,
+          item.chargingStation.longitude
+        ).then((Address) => {
+          stationAddress.innerHTML = `<strong>Address:</strong> ${Address}`;
+        });
+        ShowUserBookingCard.appendChild(stationAddress);
+
+        const chargingSlotId = document.createElement("span");
+        chargingSlotId.className = "chargingSlotId";
+        chargingSlotId.innerHTML = `<strong>Chargepoint No :</strong> ${item.chargingSlot.slotId}`;
+        ShowUserBookingCard.appendChild(chargingSlotId);
+
+        const timeSlot = document.createElement("span");
+        timeSlot.className = "timeSlot";
+        timeSlot.innerHTML = `<strong>Time:</strong> ${item.timeSlot.startTime}:00 to ${item.timeSlot.endTime}:00`;
+        ShowUserBookingCard.appendChild(timeSlot);
+
+        const bookingDate = document.createElement("span");
+        bookingDate.className = "bookingDate";
+        bookingDate.innerHTML = `<strong>Date:</strong> ${getFormattedDate(
+          item.date
+        )}`;
+        ShowUserBookingCard.appendChild(bookingDate);
+
+        const bookingStatus = document.createElement("span");
+        bookingStatus.className = "bookingStatus";
+        bookingStatus.innerHTML = "<strong>Status:</strong> ";
+        const statusText = document.createElement("span");
+        const currentDateTime = new Date();
+        const bookingDateTime = new Date(item.date);
+        bookingDateTime.setHours(item.timeSlot.startTime, 0, 0, 0);
+
+        if (currentDateTime > bookingDateTime) {
+          item.status = "Completed";
+        }
+
+        statusText.textContent = item.status;
+        if (statusText.textContent === "Confirmed") {
+          statusText.classList.add("status-confirmed");
+        } else if (statusText.textContent === "Cancelled") {
+          statusText.classList.add("status-cancelled");
+        } else if (statusText.textContent === "Completed") {
+          statusText.classList.add("status-completed");
+        }
+        bookingStatus.appendChild(statusText);
+        ShowUserBookingCard.appendChild(bookingStatus);
+
+        if (item.status === "confirmed") {
+          const cancelUserBookingBtn = document.createElement("button");
+          cancelUserBookingBtn.className = "cancelUserBookingBtn";
+          cancelUserBookingBtn.textContent = "Cancel Booking";
+          cancelUserBookingBtn.onclick = () => {
+            showModal(item.bookingId);
+          };
+          ShowUserBookingCard.appendChild(cancelUserBookingBtn);
+        }
       });
-      ShowUserBookingCard.appendChild(stationAddress);
-
-      const chargingSlotId = document.createElement('span');
-      chargingSlotId.className = 'chargingSlotId';
-      chargingSlotId.innerHTML = `<strong>Chargepoint No :</strong> ${item.chargingSlot.slotId}`;
-      ShowUserBookingCard.appendChild(chargingSlotId);
-
-      const timeSlot = document.createElement('span');
-      timeSlot.className = 'timeSlot';
-      timeSlot.innerHTML = `<strong>Time:</strong> ${item.timeSlot.startTime}:00 to ${item.timeSlot.endTime}:00`;
-      ShowUserBookingCard.appendChild(timeSlot);
-
-      const bookingDate = document.createElement('span');
-      bookingDate.className = 'bookingDate';
-      bookingDate.innerHTML = `<strong>Date:</strong> ${getFormattedDate(item.date)}`;
-      ShowUserBookingCard.appendChild(bookingDate);
-
-      const bookingStatus = document.createElement('span');
-      bookingStatus.className = 'bookingStatus';
-      bookingStatus.innerHTML = '<strong>Status:</strong> ';
-      const statusText = document.createElement('span');
-      const currentDateTime = new Date();
-      const bookingDateTime = new Date(item.date);
-      bookingDateTime.setHours(item.timeSlot.startTime, 0, 0, 0);
-
-      if (currentDateTime > bookingDateTime) {
-        item.status = 'Completed';
-      }
-
-      statusText.textContent = item.status;
-      if (statusText.textContent === 'Confirmed') {
-        statusText.classList.add('status-confirmed');
-      } else if (statusText.textContent === 'Cancelled') {
-        statusText.classList.add('status-cancelled');
-      } else if (statusText.textContent === 'Completed') {
-        statusText.classList.add('status-completed');
-      }
-      bookingStatus.appendChild(statusText);
-      ShowUserBookingCard.appendChild(bookingStatus);
-
-      if(item.status==='Confirmed') {
-        const cancelUserBookingBtn = document.createElement('button');
-        cancelUserBookingBtn.className = 'cancelUserBookingBtn';
-        cancelUserBookingBtn.textContent = 'Cancel Booking';
-        cancelUserBookingBtn.onclick = () => {
-          showModal(item.bookingId);
-        };
-        ShowUserBookingCard.appendChild(cancelUserBookingBtn);
-      }
+    })
+    .catch((error) => {
+      console.error("Error fetching user bookings:", error);
     });
-  }).catch(error => {
-    console.error('Error fetching user bookings:', error);
-  });
 }
 
 function getFormattedDate(date) {
@@ -138,22 +145,22 @@ function showModal(bookingId) {
 
   // Add event listeners
   closeButton.addEventListener("click", () => {
-      modalContainer.style.display = "none";
+    modalContainer.style.display = "none";
   });
 
   cancelButton.addEventListener("click", () => {
-      modalContainer.style.display = "none";
+    modalContainer.style.display = "none";
   });
 
   confirmButton.addEventListener("click", () => {
-      cancelBooking(bookingId);
-      modalContainer.style.display = "none";
+    cancelBooking(bookingId);
+    modalContainer.style.display = "none";
   });
 
   window.addEventListener("click", (event) => {
-      if (event.target === modalContainer) {
-          modalContainer.style.display = "none";
-      }
+    if (event.target === modalContainer) {
+      modalContainer.style.display = "none";
+    }
   });
 
   // Display the modal
